e0e521bb9f7688abc3d4025568a25136f9460b0b
Geoff Callender <geoff.callender.jumpstart@gmail.com>
2013-08-11 16:54:19 +1000

    Improved wording of the examples down to Forms, inclusive.

--- jumpstart/web/src/main/java/jumpstart/web/pages/examples/input/Input.java
+++ jumpstart/web/src/main/java/jumpstart/web/pages/examples/input/Input.java
@@ -1,5 +1,10 @@
 package jumpstart.web.pages.examples.input;
 
-
+@Deprecated
 public class Input {
+	
+	Object onActivate() {
+		return InputAndEvents.class;
+	}
+	
 }
---
+++ jumpstart/web/src/main/java/jumpstart/web/pages/examples/input/InputAndEvents.java
@@ -0,0 +1,5 @@
+package jumpstart.web.pages.examples.input;
+
+
+public class InputAndEvents {
+}
