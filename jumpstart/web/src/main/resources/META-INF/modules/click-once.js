669c63ad87a71223d27f6bdd3695571cde5bcf29
Geoff Callender <geoff.callender.jumpstart@gmail.com>
2014-11-23 18:42:41 +1100

    Improved click-once comments.

--- jumpstart/web/src/main/resources/META-INF/modules/click-once.js
+++ jumpstart/web/src/main/resources/META-INF/modules/click-once.js
@@ -1,4 +1,6 @@
-// A class that ignores clicks after the first one.
+// A module that ignores clicks after the first one.
+// For each clickable element that you want to participate, call init. 
+// Once any of the elements is clicked, this module will cancel subsequent clicks from any of the elements.
 
 define([ "jquery" ], function($) {
 	
