812136d4bd73ae19286edff4f1453dce3e836970
Geoff Callender <geoff.callender.jumpstart@gmail.com>
2018-05-16 20:39:47 +0800

    Index: renamed category "Input and Events" to "Input".

--- web/src/main/java/jumpstart/web/pages/examples/input/InputAndEvents.java
+++ web/src/main/java/jumpstart/web/pages/examples/input/InputAndEvents.java
@@ -1,5 +1,10 @@
 package jumpstart.web.pages.examples.input;
 
-
+@Deprecated
 public class InputAndEvents {
+
+	Object onActivate() {
+		return jumpstart.web.pages.examples.input.RequestTypes.class;
+	}
+
 }
